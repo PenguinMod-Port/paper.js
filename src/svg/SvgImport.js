fdacf6efa2ea4a36146fe779ce6097dff0af2ecd
jwklong <jwklong@gmx.com>
2026-09-24 16:03:21 +0100

    this is very limiting
    Signed-off-by: jwklong <jwklong@gmx.com>

--- src/svg/SvgImport.js
+++ src/svg/SvgImport.js
@@ -722,7 +722,7 @@ new function() {
             settings.applyMatrix = applyMatrix;
             if (item) {
                 // pm: naming support
-                for (let node of childNodes) {
+                for (var node of childNodes) {
                     console.log(node)
                 }
 
